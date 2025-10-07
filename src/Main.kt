f8599f614dcf8738ea486dbd10a53485b98ca0fd
Zadacha-1_mobile-app_kt <gsolod07@gamil.com>
2025-10-07 12:09:06 +0300

    задача 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("Введите строку:")
+    val input = readLine()!!
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    var result = ""
+    var i = 0
+
+    while (i < input.length) {
+        val currentChar = input[i]
+        var count = 1
+
+        while (i + count < input.length && input[i + count] == currentChar) {
+            count++//считаем повторы
+        }
+
+        result += if (count > 1) "$currentChar$count" else "$currentChar"//если повторов больше 1 ставим цифру
+        i += count
     }
+
+
+    println("Результат: $result")
 }
